a86ee2c0ea998a34716b8034792ad5fb5f072127
Deepak <deepaksingh24081996@gmail.com>
2020-07-08 21:52:35 +0530

    done

--- dictionaryUI/src/app/app.component.ts
+++ dictionaryUI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  word: string;
   title = 'dictionaryUI';
+  submitCliked = false;
+  apiURL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  meaningBody: any;
+  errorMessage: string;
+  hasError = false;
+
+  constructor(private http: HttpClient){
+  }
+
+  searchMeaning(){
+    this.hasError = false;
+    const Url: string = this.apiURL + this.word;
+    this.http.get(Url).subscribe(data => {
+      console.log(this.word);
+      if (data){
+        this.submitCliked = true;
+        this.meaningBody = data;
+        console.log(this.meaningBody);
+      }
+
+    }, (error => {
+      console.log(error);
+      this.handleError();
+    }));
+  }
+  exaplePresent(example: any){
+    if (example){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  handleError(){
+    this.hasError = true;
+    this.errorMessage = 'Something went Wrong';
+  }
 }
